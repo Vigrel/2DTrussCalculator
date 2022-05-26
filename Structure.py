6350399f6a70473df9443cec24ca44548ce687d7
vigrel <eller.vini@gmail.com>
2022-05-26 09:12:27 -0300

    feat(file): add create elements func in Structure class

--- Structure.py
+++ Structure.py
@@ -18,5 +18,22 @@ class Structure():
         self.F  : np.array[List[float]] = np.array(data[5])
         self.R  : np.array[List[float]] = np.array(data[7])
 
-    def create_elements() -> List[Element]:
-        pass
+        self.elementos: List[Element] = self.create_elements()
+
+    def create_elements(self) -> List[Element]:
+        elementos = []
+
+        for i in range(self.N):
+            ix_n1 = int(self.Inc[i][0])
+            node1 = Node(self.N[0][ix_n1 - 1],
+                         self.N[1][ix_n1 - 1])
+
+            ix_n1 = int(self.Inc[i][1])
+            node2 = Node(self.N[0][ix_n1 - 1],
+                         self.N[1][ix_n1 - 1])
+
+            youngs_module = self.Inc[i][2]
+            area = self.Inc[i][3]
+            
+            elementos.append(Element(node1, node2, youngs_module, area, i))
+        return elementos
