134a96963b4f5143b9dc757a8ed3528d9aa095b5
vigrel <eller.vini@gmail.com>
2022-05-27 11:55:09 -0300

    fix(file): add node dof's || calculate global stiffness matrix

--- Structure.py
+++ Structure.py
@@ -18,22 +18,42 @@ class Structure():
         self.F  : np.array[List[float]] = np.array(data[5])
         self.R  : np.array[List[float]] = np.array(data[7])
 
-        self.elementos: List[Element] = self.create_elements()
+        self.elements_list: List[Element] = self.create_elements()
+        self.global_stiffness_matrix: None = self.create_global_stiffness_matrix()
 
     def create_elements(self) -> List[Element]:
-        elementos = []
+        elements = []
 
-        for i in range(self.N):
+        for i in range(self.nn):
             ix_n1 = int(self.Inc[i][0])
             node1 = Node(self.N[0][ix_n1 - 1],
-                         self.N[1][ix_n1 - 1])
+                         self.N[1][ix_n1 - 1],
+                         ix_n1 * 2 - 1,
+                         ix_n1 * 2)
 
-            ix_n1 = int(self.Inc[i][1])
-            node2 = Node(self.N[0][ix_n1 - 1],
-                         self.N[1][ix_n1 - 1])
+            ix_n2 = int(self.Inc[i][1])
+            node2 = Node(self.N[0][ix_n2 - 1],
+                         self.N[1][ix_n2 - 1],
+                         ix_n2 * 2 - 1,
+                         ix_n2 * 2)
 
             youngs_module = self.Inc[i][2]
             area = self.Inc[i][3]
             
-            elementos.append(Element(node1, node2, youngs_module, area, i))
-        return elementos
+            elements.append(Element(node1, node2, youngs_module, area))
+        return elements
+    
+    def create_global_stiffness_matrix(self) -> None:
+        base_matrix = np.zeros((self.nn*2, self.nn*2))
+
+        for element in self.elements_list:
+            base_matrix[element.node1.dofx - 1 : element.node1.dofy,
+                        element.node1.dofx - 1 : element.node1.dofy] += element.stiffness[:2, :2]
+            base_matrix[element.node1.dofx - 1 : element.node1.dofy,
+                        element.node2.dofx - 1 : element.node2.dofy] += element.stiffness[2:, :2]
+            base_matrix[element.node2.dofx - 1 : element.node2.dofy,
+                        element.node1.dofx - 1 : element.node1.dofy] += element.stiffness[:2, 2:]
+            base_matrix[element.node2.dofx - 1 : element.node2.dofy,
+                        element.node2.dofx - 1 : element.node2.dofy] += element.stiffness[2:, 2:]
+
+        return base_matrix
