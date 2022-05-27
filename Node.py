64e55af3d12036125fdc2de00eb885306ae995ca
vigrel <eller.vini@gmail.com>
2022-05-27 11:52:32 -0300

    feat(file): add degrees of freedom to Node class

--- Node.py
+++ Node.py
@@ -1,4 +1,6 @@
 class Node():
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, dofx: int, dofy: int):
         self.x: int = x
         self.y: int = y
+        self.dofx: int = dofx
+        self.dofy: int = dofy
