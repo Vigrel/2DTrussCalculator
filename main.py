8d45220a8327774dacf640ecccc5ee83c0ea9f20
vigrel <eller.vini@gmail.com>
2022-05-31 18:59:00 -0300

    feat(file): add main || run code with args

---
+++ main.py
@@ -0,0 +1,19 @@
+from Structure import Structure
+import funcoesTermosol
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    requiredArgs = parser.add_argument_group("required arguments")
+    requiredArgs.add_argument("-i", "--input", required=True)
+    args = parser.parse_args()
+
+    structure = Structure(args.input)
+    funcoesTermosol.geraSaida(
+        "saida.txt",
+        structure.support_reactions,
+        structure.displacement,
+        structure.deformation,
+        structure.internal_forces,
+        structure.truss,
+    )
