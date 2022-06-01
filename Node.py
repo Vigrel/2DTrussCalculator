8a7ce22c5dcfc1d3ea7ea901fb00ed25cf4f4730
vigrel <eller.vini@gmail.com>
2022-05-31 23:09:14 -0300

    docs(file): node class documentation

--- Node.py
+++ Node.py
@@ -1,5 +1,35 @@
 class Node:
-    def __init__(self, x: int, y: int, dofx: int, dofy: int):
+    """
+    A class to represent a Node. In this case, Node is a point on a 2D plane.
+    ...
+
+    Attributes
+    ----------
+    x : Node
+        Location on the x axis 
+    y : Node
+        Location on the y axis 
+    dofx : float
+        Degrees of Freedom on the x axis 
+    dofy : float
+        Degrees of Freedom on the y axis 
+    """
+    def __init__(self, x: int, y: int, dofx: int, dofy: int) -> None:
+        """
+        Constructs all the necessary attributes for the Node object.
+
+        Parameters
+        ----------
+        x : Node
+            Location on the x axis 
+        y : Node
+            Location on the y axis 
+        dofx : float
+            Degrees of Freedom on the x axis 
+        dofy : float
+            Degrees of Freedom on the y axis 
+        """
+
         self.x: int = x
         self.y: int = y
         self.dofx: int = dofx
